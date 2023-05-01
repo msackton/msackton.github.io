c1c45deae16ba99c0cf4fec17b23bc8acfb3717c
Mike Sackton <msackton@eschergroup.com>
2023-05-01 19:26:16 -0400

    Auto-publish of converted html

--- taelgar1720/obs.html/static/obsidian_core.js
+++ taelgar1720/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/taelgar1720";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '322152270433024469523290720024329413040'                       // used to check whether the localStorage data is stale
+var gzip_hash = '301118322418366833937030179777413721432'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
